b3dbe0f5f8ad6d19d3391255c8c8f4dbdccee259
earthnaut <earthnaut999@gmail.com>
2023-11-09 14:23:46 +0900

    django create & init setting

---
+++ django_your_voice/django_your_voice/settings/local.py
@@ -0,0 +1,22 @@
+from .common import *
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(+30(rz2$l+lfbm&)oe_7s+pitlr4!-6w!86+)&1y7)9a5f@6v"
+
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'your_voice',
+        'USER': 'admin',
+        'PASSWORD': '000000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}

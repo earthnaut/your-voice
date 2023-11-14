cff1eb294d035a47140da8763d2b6169fccddaba
earthnaut <earthnaut999@gmail.com>
2023-11-14 22:06:06 +0900

    html - loading progress bar edit but not sueccess

--- django_your_voice/home/views.py
+++ django_your_voice/home/views.py
@@ -9,8 +9,56 @@ def upload_media(request):
 def select_language(request):
     return render(request, 'home/select_language.html', {})
 
+
+
+# 모델예상시간 로딩바 구현하기위한 라이브러리
+# case 1 : 웹홈페이지 제공이랑 모델구현(+예상시간)이랑 하나로 합친거
+# case 1 문제점 : 모델이 돌아가는 동안 http://loading/ 창 표시를 안함!!!
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from tqdm import tqdm
+@csrf_exempt
+def loading(request):
+
+    # ~~~ 모델 실행 및 예상 시간 계산 로직 추가 ~~~
+    # (걸리는 시간만 확인하려고 만든 모델임 나중에 지워도 됨)
+    import time
+    progress_bar = tqdm(range(10))
+    for i in progress_bar:
+        time.sleep(1)
+
+    # ~~~ 예상 시간 및 진행률을 클라이언트에게 반환 ~~~
+    progress = progress_bar.format_dict['n'] / progress_bar.format_dict['total']  # 현재까지 pqdm이 실행된 n횟수를 반환함
+    estimated_time = progress_bar.format_dict['elapsed']  # 예상 시간
+
+    return render(request, 'home/loading.html', {'progress':progress,
+                                                 'estimated_tmie':estimated_time,})
+
+
+
+# case 2 : 웹홈페이지 제공이랑 모델구현(+예상시간)을 분리
+
+# 웹홈페이지 제공
 def loading(request):
     return render(request, 'home/loading.html', {})
+
+# 모델구현 + 예상시간
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from tqdm import tqdm
+@csrf_exempt
+def run_model(request):
+    # 모델 실행 및 예상 시간 계산 로직 추가
+
+    # 예상 시간 및 진행률을 클라이언트에게 반환
+    response_data = {
+        'progress': 50,  # 예시로 50%로 설정
+        'estimated_time': '2 hours',  # 예상 시간
+    }
+
+    return JsonResponse(response_data)
+
+
 
 def select_speaker(request):
     speaker_num = [1, 2, 3]
